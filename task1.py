488a95170320b68ea8cae785c67fb399d5cb3b67
Denis Bazhenov <denkolobusik@gmail.com>
2025-12-09 15:40:19 +0200

    зробив половину першого завдання

---
+++ task1.py
@@ -0,0 +1,15 @@
+book_profile = {
+    "title": "основи програмування",
+    "author": "Іван петренко",
+    "year": 2023,
+    "publisher_info": {
+        "name": "Наукова думка",
+        "city": "Київ"
+    }
+
+}
+print(book_profile["title"])
+print(book_profile["author"])
+
+print(book_profile["publisher_info"]["name"])
+print("Книга", book_profile["author"], "була видана у місті", book_profile["publisher_info"]["city"])
